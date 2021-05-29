cafb2131a4ee71a5f98a22549c902716dc941685
Yukwok <billynyk@gmail.com>
2021-05-29 22:58:12 +0800

    first python

---
+++ main.py
@@ -0,0 +1,21 @@
+import tkinter
+
+window = tkinter.Tk()
+window.title('Billy first GUI app')
+window.minsize(width=500, height=300)
+print('hello world')
+
+
+
+
+label = tkinter.Label(text='i am a label1,s sfsdf')
+
+label.pack()
+
+
+window.mainloop()
+
+
+
+
+
