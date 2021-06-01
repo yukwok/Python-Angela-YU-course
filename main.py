86a7a9c75b9bc700f2653c0533f7ec8647c317c0
Yukwok <billynyk@gmail.com>
2021-06-01 16:39:10 +0800

    *args vs **kwarg learning

--- main.py
+++ main.py
@@ -6,16 +6,16 @@ window.minsize(width=500, height=300)
 print('hello world')
 
 
-
-
 label = tkinter.Label(text='i am a label1,s sfsdf')
 
 label.pack()
 
 
+def all_aboard(a, *args, **kw):
+    print(a, args, kw)
+
+
+all_aboard(4, 7, 3, 0, x=10, y=64, )
+
+
 window.mainloop()
-
-
-
-
-
